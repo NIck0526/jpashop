1732d024333bf33cf1b695983cb4db3a29abd38d
NIck0526 <km.lee0526@gmail.com>
2020-12-08 00:21:07 +0900

    주문 서비스 개발 완료

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,7 +1,9 @@
 package jpabook.jpashop.domain;
 
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -9,6 +11,7 @@ import javax.persistence.*;
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED) /* 엔터티 객체를 생성한 뒤 setter를 독립적으로 생성못하게 막음 (createOrderItem)과 같이 메서드 생성하여 사용*/
 public class OrderItem {
 
     @Id
@@ -26,6 +29,7 @@ public class OrderItem {
 
     private int orderPrice; //주문 가격
     private int count; //주문 수량
+
 
     /* ==생성 메서드== */
     public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
@@ -46,7 +50,7 @@ public class OrderItem {
     /* ==조회 로직== */
 
     /**
-     * 주문상품 전체 가격 조회회
+     * 주문상품 전체 가격 조회
      */
     public int getTotalPrice() {
         return getOrderPrice() * getCount();
